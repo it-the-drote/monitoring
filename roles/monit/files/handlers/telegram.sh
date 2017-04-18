ba8f220b7bc48d9dc5fe3c8d5dcaeec222ce927a
Azer Abdullaev <reactorcoremeltdown@Azers-MBP.6min.lan>
2017-04-18 16:26:11 +0200

    Updated the notification script

--- roles/monit/files/handlers/telegram.sh
+++ roles/monit/files/handlers/telegram.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
+source /usr/lib/leicht/leicht.sh
+
+text=""
+
+case $MONIT_PROGRAM_STATUS in
+  "0")
+    text="✅✅✅ $MONIT_DESCRIPTION"
+    ;;
+  "1")
+    text="⚠️⚠️⚠️ $MONIT_DESCRIPTION"
+    ;;
+  "2")
+    text="❌❌❌ $MONIT_DESCRIPTION"
+esac
+
+leicht_send_message `cat /etc/datasources/pisun-default-chat` "0" "[sensu] $text" "false" /tmp/pisun.socket
 exit 0
