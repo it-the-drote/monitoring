0f559a346375f65d834b8a9bff7f1501c7f5ead1
Azer Abdullaev <reactorcoremeltdown@Azers-MacBook-Pro.local>
2017-04-18 23:00:20 +0200

    Added basic plugins

---
+++ roles/monit/files/plugins/check_https_certificate.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source /etc/monit/plugins/okfail.sh
+
+pubdate=`echo | openssl s_client -connect $1':443' 2>/dev/null | openssl x509 -noout -dates | grep notAfter | cut -f 2 -d '='`
+pubdate_unix=`date +%s -d "$pubdate"`
+current_date=`date +%s`
+let delta="pubdate_unix - current_date"
+let days="delta/86400"
+
+if test $delta -lt 2592000; then
+    fail "The certificate of $1 will expire in $days days!"
+else
+    ok "Certificate is ok."
+fi
