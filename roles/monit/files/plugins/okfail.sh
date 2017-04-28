d246f5c82ce2f1cf0d7c7c5de21de3a590128943
Azer Abdullaev <reactorcoremeltdown@Azers-MBP.6min.lan>
2017-04-28 14:46:08 +0200

    Fixed a typo in mongodb command.

--- roles/monit/files/plugins/okfail.sh
+++ roles/monit/files/plugins/okfail.sh
@@ -3,7 +3,7 @@ ENVIRONMENT=$3
 
 function ok() {
     if [[ $3 = 'external' ]]; then
-      mongo --quiet --port 27018 monitoring --eval 'db.checks.update({name: "'"$2"'"}, {name: "Nextcloud HTTPS", status: 0}, { upsert: true })'
+      mongo --quiet --port 27018 monitoring --eval 'db.checks.update({name: "'"$2"'"}, {name: "'"$2"'", status: 0}, { upsert: true })'
     fi
     echo -n "OK - "$1
     exit 0
@@ -11,7 +11,7 @@ function ok() {
 
 function warning() {
     if [[ $3 = 'external' ]]; then
-      mongo --quiet --port 27018 monitoring --eval 'db.checks.update({name: "'"$2"'"}, {name: "Nextcloud HTTPS", status: 1}, { upsert: true })'
+      mongo --quiet --port 27018 monitoring --eval 'db.checks.update({name: "'"$2"'"}, {name: "'"$2"'", status: 1}, { upsert: true })'
     fi
     echo -n "WARNING - "$1
     exit 1
@@ -19,7 +19,7 @@ function warning() {
 
 function fail() {
     if [[ $3 = 'external' ]]; then
-      mongo --quiet --port 27018 monitoring --eval 'db.checks.update({name: "'"$2"'"}, {name: "Nextcloud HTTPS", status: 2}, { upsert: true })'
+      mongo --quiet --port 27018 monitoring --eval 'db.checks.update({name: "'"$2"'"}, {name: "'"$2"'", status: 2}, { upsert: true })'
     fi
     echo -n "CRITICAL - "$1
     exit 2
