487d1595e153c8925978a78b4aa74cae4e17c099
Azer Abdullaev <azer.abdullaev@protonmail.ch>
2017-05-11 15:14:50 +0200

    Changed time constraints for TLS certificates

--- roles/monit/files/plugins/check_https_certificate.sh
+++ roles/monit/files/plugins/check_https_certificate.sh
@@ -8,10 +8,10 @@ current_date=`date +%s`
 let delta="pubdate_unix - current_date"
 let days="delta/86400"
 
-if test $delta -lt 2592000; then
+if test $delta -lt 864000; then
     warning "The certificate of $1 will expire in $days days!" "$DESCRIPTION" "$ENVIRONMENT"
-elif test $delta -lt 0; then
-    fail "The certificate of $1 has expired!" "$DESCRIPTION" "$ENVIRONMENT"
+elif test $delta -lt 259200; then
+    fail "The certificate of $1 will expire in $days days!" "$DESCRIPTION" "$ENVIRONMENT"
 else
     ok "Certificate is ok." "$DESCRIPTION" "$ENVIRONMENT"
 fi
