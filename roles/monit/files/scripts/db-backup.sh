56b696fae0a9914da6c772e2d9e8f128026f2001
Azer Abdullaev <azer.abdullaev@protonmail.ch>
2017-12-08 14:20:33 +0100

    Added logging & backup rotation

--- roles/monit/files/scripts/db-backup.sh
+++ roles/monit/files/scripts/db-backup.sh
@@ -8,9 +8,11 @@ source $ENV_FILE
 
 for i in $MYSQL_BACKUPS; do
   DUMP_FILE="${i}-`date '+%Y_%m_%d'`.sql"
-  cd /tmp && mysqldump -u $MYSQL_USERNAME -p$MYSQL_PASSWORD $i > $DUMP_FILE
-  mcrypt $DUMP_FILE -k $ENCRYPTION_PASSWORD -z
-  mv "${DUMP_FILE}.gz.nc" "${BACKUP_DIRECTORY}/${i}"
+  cd /tmp && mysqldump -u $MYSQL_USERNAME -p$MYSQL_PASSWORD $i > $DUMP_FILE && echo "MySQLdump OK"
+  mcrypt $DUMP_FILE -k $ENCRYPTION_PASSWORD -z && echo "Mcrypt OK"
+  mv "${DUMP_FILE}.gz.nc" "${BACKUP_DIRECTORY}/${i}" && "Uploading ${DUMP_FILE} to Disk OK"
   rm $DUMP_FILE
   sync
+  OLD_FILE="${i}-`date --date='7 days ago' '+%Y_%m_%d'`.sql"
+  rm "${BACKUP_DIRECTORY}/${OLD_FILE}" && "Old backups rotation OK"
 done
