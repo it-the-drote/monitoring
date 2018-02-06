7093eec852c19eb32ecf28e942e4d4671e2d2f48
Azer Abdullaev <azer.abdullaev@protonmail.ch>
2018-02-06 07:31:31 +0100

    Added routines for automated certificate renewal

--- roles/monit/files/plugins/check_https_certificate.sh
+++ roles/monit/files/plugins/check_https_certificate.sh
@@ -11,6 +11,16 @@ let days="delta/86400"
 if test $delta -lt 864000; then
     warning "The certificate of $1 will expire in $days days!" "$DESCRIPTION" "$ENVIRONMENT"
 elif test $delta -lt 259200; then
+    #### CERTIFICATE REISSUE BLOCK
+    echo -e "~~~~~~~~~~\nATTEMPTING TO ISSUE A CERTIFICATE FOR ${1}\n~~~~~~~~~~\n" >> /var/log/acme.log
+    /etc/monit/scripts/acme.sh --issue -d "$1" -w /var/www/acme >> /var/log/acme.log
+    echo -e "~~~~~~~~~~\nATTEMPTING TO INSTALL A CERTIFICATE FOR ${1}\n~~~~~~~~~~\n" >> /var/log/acme.log
+    /etc/monit/scripts/acme.sh --install-cert -d "$1" \
+                                --cert-file "/etc/ssl/acme/${1}-cert.pem" \
+                                --key-dile "/etc/ssl/acme/${1}-key.pem" \
+                                --fullchain-file "/etc/ssl/acme/${1}-fullchain.pem" \
+                                --reloadcmd "systemctl restart nginx.service" >> /var/log/acme.log
+    #### CERTIFICATE REISSUE BLOCK
     fail "The certificate of $1 will expire in $days days!" "$DESCRIPTION" "$ENVIRONMENT"
 else
     ok "Certificate is ok." "$DESCRIPTION" "$ENVIRONMENT"
