3f30af2da5142a543f87736c311c3e2d574eab8f
Azer Abdullaev <azer.abdullaev@protonmail.ch>
2018-02-18 22:36:32 +0100

    Updated disk space warning message

--- roles/monit/files/plugins/check_disk_space.sh
+++ roles/monit/files/plugins/check_disk_space.sh
@@ -7,5 +7,5 @@ usage=`df $1 | tail -n 1 | awk '{print $5}' | sed 's|%||'`
 if [[ $usage -lt 95 ]]; then
     ok "enough free space on disk"
 else
-    fail "only $usage% of disk space left"
+    fail "The amount of used disk space on $1 is $usage%"
 fi
