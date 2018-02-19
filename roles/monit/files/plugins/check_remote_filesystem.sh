aadd056aeca0c07aea1be33515b86c9562d6eff6
Azer Abdullaev <azer.abdullaev@protonmail.ch>
2018-02-20 00:22:48 +0100

    Updated remote fs script check

--- roles/monit/files/plugins/check_remote_filesystem.sh
+++ roles/monit/files/plugins/check_remote_filesystem.sh
@@ -2,9 +2,7 @@
 
 source /etc/monit/plugins/okfail.sh
 
-status=`curl -A "monit-ping-check" -s -o /dev/null -w "%{http_code}" --connect-timeout 5 --max-time 5 "file://${1}/health.json`
-
-if [[ $status == "200" ]]; then
+if curl --fail -A "monit-ping-check" -s -o /dev/null -w "%{http_code}" --connect-timeout 5 --max-time 5 "file://${1}/health.json"; then
     ok "Remote filesystem at ${1} is accessible"
 else
     fail "Remote filesystem at ${1} is inaccessible!"
