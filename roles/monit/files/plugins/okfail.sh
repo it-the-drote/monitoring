33ae024e8dc108c1a329c75e73c520cb2a38e915
Azer Abdullaev <azer.abdullaev@protonmail.ch>
2018-05-28 08:01:40 +0200

    Introduced global failure counters

--- roles/monit/files/plugins/okfail.sh
+++ roles/monit/files/plugins/okfail.sh
@@ -1,11 +1,13 @@
 DESCRIPTION=$2
 ENVIRONMENT=$3
 
+counter_file="/var/lib/monit/counters/"`basename -s '.sh' ${0}`"-${4}"
 
 function ok() {
     if [[ $3 = 'external' ]]; then
       mongo --quiet --port 27018 monitoring --eval 'db.checks.update({name: "'"$2"'"}, {name: "'"$2"'", status: 0}, { upsert: true })'
     fi
+    echo "0" > $counter_file
     echo -n "OK - "$1
     exit 0
 }
@@ -22,6 +24,12 @@ function fail() {
     if [[ $3 = 'external' ]]; then
       mongo --quiet --port 27018 monitoring --eval 'db.checks.update({name: "'"$2"'"}, {name: "'"$2"'", status: 2}, { upsert: true })'
     fi
-    echo -n "CRITICAL - "$1
-    exit 2
+    counter=`cat ${counter_file}`
+    let 'counter=counter+1'
+    echo $counter > $counter_file
+
+    if [[ "$counter" -gt "2" ]]; then
+      echo -n "CRITICAL - "$1
+      exit 2
+    fi
 }
