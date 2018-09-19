ef2deb5f08a96f767d8c76222a702831d723712c
Azer Abdullaev <azer.abdullaev@protonmail.ch>
2018-09-19 09:11:03 +0200

    Added a real timeout to check

--- roles/monit/files/plugins/check_remote_filesystem.sh
+++ roles/monit/files/plugins/check_remote_filesystem.sh
@@ -2,7 +2,7 @@
 
 source /etc/monit/plugins/okfail.sh
 
-if curl --fail -A "monit-ping-check" -s -o /dev/null -w "%{http_code}" --connect-timeout 5 --max-time 5 "file://${1}/health.json"; then
+if timeout 10 curl --fail -A "monit-ping-check" -s -o /dev/null -w "%{http_code}" --connect-timeout 5 --max-time 5 "file://${1}/health.json"; then
     ok "Remote filesystem at ${1} is accessible"
 else
     fail "Remote filesystem at ${1} is inaccessible!"
