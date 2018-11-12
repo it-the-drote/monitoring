49f4e0408063727b0d46df41acf285b84f43f19a
Azer Abdullaev <azer.abdullaev@protonmail.ch>
2018-11-12 13:03:28 +0100

    Removed some junk

--- roles/monit/files/handlers/telegram.sh
+++ roles/monit/files/handlers/telegram.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-source /etc/monit/handlers/tickets.sh
-
-login_url=`cat /etc/datasources/http2ssh_login.txt`
 text=""
 tail="\\\\nCheck name: $MONIT_SERVICE\\\\nDescription: $MONIT_DESCRIPTION"
 
@@ -11,10 +8,10 @@ case $MONIT_PROGRAM_STATUS in
     text="✅✅✅\\\\n$tail"
     ;;
   "1")
-    text="⚠️⚠️⚠️\\\\n$tail\\\\n\\\\nLogin: $login_url"
+    text="⚠️⚠️⚠️\\\\n$tail"
     ;;
   "2")
-    text="❌❌❌\\\\n$tail\\\\n\\\\nLogin: $login_url"
+    text="❌❌❌\\\\n$tail"
 esac
 
 echo -e '{ "actionType": "SendMessage", "actionSettings": {"chatID": '`cat /etc/datasources/pisun-default-chat`', "replyToMessageID": 0, "text": "'"$text"'", "disableWebPagePreview": true }}' | socat stdio unix-connect:/var/run/apps/pisun.socket
--- roles/monit/files/plugins/okfail.sh
+++ roles/monit/files/plugins/okfail.sh
@@ -4,26 +4,17 @@ ENVIRONMENT=$3
 counter_file="/var/lib/monit/counters/"`basename -s '.sh' ${0}`"-${4}"
 
 function ok() {
-    if [[ $3 = 'external' ]]; then
-      mongo --quiet --port 27018 monitoring --eval 'db.checks.update({name: "'"$2"'"}, {name: "'"$2"'", status: 0}, { upsert: true })'
-    fi
     echo "0" > $counter_file
     echo -n "OK - "$1
     exit 0
 }
 
 function warning() {
-    if [[ $3 = 'external' ]]; then
-      mongo --quiet --port 27018 monitoring --eval 'db.checks.update({name: "'"$2"'"}, {name: "'"$2"'", status: 1}, { upsert: true })'
-    fi
     echo -n "WARNING - "$1
     exit 1
 }
 
 function fail() {
-    if [[ $3 = 'external' ]]; then
-      mongo --quiet --port 27018 monitoring --eval 'db.checks.update({name: "'"$2"'"}, {name: "'"$2"'", status: 2}, { upsert: true })'
-    fi
     counter=`cat ${counter_file}`
     let 'counter=counter+1'
     echo $counter > $counter_file
