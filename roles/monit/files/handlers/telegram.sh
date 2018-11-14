e845582cd00db3b1dc27e9f3652b225b56a9d827
Azer Abdullaev <azer.abdullaev@protonmail.ch>
2018-11-14 08:27:28 +0100

    Socket path has changed

--- roles/monit/files/handlers/telegram.sh
+++ roles/monit/files/handlers/telegram.sh
@@ -14,5 +14,5 @@ case $MONIT_PROGRAM_STATUS in
     text="❌❌❌\\\\n$tail"
 esac
 
-echo -e '{ "actionType": "SendMessage", "actionSettings": {"chatID": '`cat /etc/datasources/pisun-default-chat`', "replyToMessageID": 0, "text": "'"$text"'", "disableWebPagePreview": true }}' | socat stdio unix-connect:/var/run/apps/pisun.socket
+echo -e '{ "actionType": "SendMessage", "actionSettings": {"chatID": '`cat /etc/datasources/pisun-default-chat`', "replyToMessageID": 0, "text": "'"$text"'", "disableWebPagePreview": true }}' | socat stdio unix-connect:/var/run/apps/pisun.sock
 exit 0
