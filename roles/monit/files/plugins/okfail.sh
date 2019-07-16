b7c411dca37ac18f19888476399cab7dc6bbc21f
Azer Abdullaev <azer.abdullaev@protonmail.ch>
2019-07-16 12:16:40 +0200

    Made monitoring stateless

--- roles/monit/files/plugins/okfail.sh
+++ roles/monit/files/plugins/okfail.sh
@@ -1,8 +1,6 @@
 DESCRIPTION=$2
 ENVIRONMENT=$3
 
-counter_file="/var/lib/monit/counters/"`basename -s '.sh' ${0}`"-${4}"
-
 function ok() {
     echo "0" > $counter_file
     echo -n "OK - "$1
@@ -15,12 +13,6 @@ function warning() {
 }
 
 function fail() {
-    counter=`cat ${counter_file}`
-    let 'counter=counter+1'
-    echo $counter > $counter_file
-
-    if [[ "$counter" -gt "2" ]]; then
-      echo -n "CRITICAL - "$1
-      exit 2
-    fi
+    echo -n "CRITICAL - "$1
+    exit 2
 }
