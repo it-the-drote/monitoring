c4911e622b04f1d55e78c7b449336686ebdc18fb
Azer <rcmd@caesium.local>
2020-01-07 15:09:01 +0100

    Added memory usage plugin

--- roles/monit/files/scripts/build_graphs.sh
+++ roles/monit/files/scripts/build_graphs.sh
@@ -8,3 +8,7 @@ rrdtool graph /var/www/healthchecks/builds/operations/loadavg_5min.png \
     -w 1024 -h 512 \
     DEF:loadavg=/var/storage/wastebox/operations/loadavg_5min.rrd:loadavg:AVERAGE \
     LINE2:loadavg#0000ff:"Load average for 5 minutes"
+rrdtool graph /var/www/healthchecks/builds/operations/memory_usage.png \
+    -w 1024 -h 512 \
+    DEF:loadavg=/var/storage/wastebox/operations/memory_usage.rrd:memory_usage:AVERAGE \
+    LINE2:memory_usage#0000ff:"Memory usage"
