ba46584c62a9837bb735fce98092ca6fffa0282d
Azer Abdullaev <azer.abdullaev@protonmail.ch>
2020-05-22 23:36:58 +0200

    Added severity level support to squadcast alerts

--- roles/monit/files/handlers/squadcast.sh
+++ roles/monit/files/handlers/squadcast.sh
@@ -6,15 +6,15 @@ severity="low"
 
 case $MONIT_PROGRAM_STATUS in
   "1")
-    severity="medium"
+    severity="#FFFF00"
     ;;
   "2")
-    severity="high"
+    severity="#FF0000"
     ;;
 esac
 
 curl -X POST "https://api.squadcast.com/v2/incidents/api/${SQUADCAST_TOKEN}" \
     -H 'Content-Type: application/json' \
-    -d "{ \"message\": \"${MONIT_SERVICE}\", \"description\": \"${MONIT_DESCRIPTION}\" }"
+    -d "{ \"message\": \"${MONIT_SERVICE}\", \"description\": \"${MONIT_DESCRIPTION}\", \"severity\": {\"colour\": \"${severity}\"} }"
 
 exit 0
