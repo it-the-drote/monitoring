9cae0509ff2bab2c3acd5b3d60fae0860565145d
Azer Abdullaev <azer.abdullaev@protonmail.ch>
2020-05-25 16:57:35 +0200

    Added support of automatically resolving incidents

--- roles/monit/files/handlers/squadcast.sh
+++ roles/monit/files/handlers/squadcast.sh
@@ -2,19 +2,23 @@
 
 source /etc/datasources/squadcast.env
 
-severity="low"
+EVENT_FILE="/var/lib/monit/events/${MONIT_SERVICE}"
 
 case $MONIT_PROGRAM_STATUS in
-  "1")
-    severity="#FFFF00"
+  "0")
+    EVENT_ID=$(cat ${EVENT_FILE})
+    curl -X POST "https://api.squadcast.com/v2/incidents/api/${SQUADCAST_TOKEN}" \
+        -H 'Content-Type: application/json' \
+        -d "{ \"status\": \"resolved\", \"event_id\": \"${EVENT_ID}\" }"
+    echo "" > ${EVENT_FILE}
     ;;
-  "2")
-    severity="#FF0000"
+  "1"|"2")
+    uuidgen > $EVENT_FILE
+    EVENT_ID=$(cat ${EVENT_FILE})
+    curl -X POST "https://api.squadcast.com/v2/incidents/api/${SQUADCAST_TOKEN}" \
+        -H 'Content-Type: application/json' \
+        -d "{ \"message\": \"${MONIT_SERVICE}\", \"description\": \"${MONIT_DESCRIPTION}\", \"status\": \"trigger\", \"event_id\": \"${EVENT_ID}\" }"
     ;;
 esac
-
-curl -X POST "https://api.squadcast.com/v2/incidents/api/${SQUADCAST_TOKEN}" \
-    -H 'Content-Type: application/json' \
-    -d "{ \"message\": \"${MONIT_SERVICE}\", \"description\": \"${MONIT_DESCRIPTION}\", \"severity\": {\"colour\": \"${severity}\"} }"
 
 exit 0
